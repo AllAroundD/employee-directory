a1876e5ce0674b3ad28ab931d8bc319ccc0323ee
AllAroundD <AllAroundDWeb@gmail.com>
2020-07-26 22:36:59 -0400

    modified gitignore to include build

---
+++ build/precache-manifest.f098f095a30da770c894be686f2d2580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afaf933b7dc1105d5bd5a1b51898a551",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf9899bcc17784112612",
+    "url": "/static/css/main.24e668de.chunk.css"
+  },
+  {
+    "revision": "3879444b81a8f54c7c19",
+    "url": "/static/js/2.dc380b87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dc380b87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9899bcc17784112612",
+    "url": "/static/js/main.de6d0ce9.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
